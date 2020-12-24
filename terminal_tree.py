9cc611560a7114ba9b9b430395033896a27bd2c6
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-12-23 23:21:52 -0300

    CLI

--- terminal_tree.py
+++ terminal_tree.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import random
 import time
@@ -5,10 +6,7 @@ import time
 BALL = '⏺'
 COLOR = {
     'blue': '\033[94m',
-    # 'default': '\033[99m',
-    # 'grey': '\033[90m',
     'yellow': '\033[93m',
-    # 'black': '\033[90m',
     'cyan': '\033[96m',
     'green': '\033[92m',
     'magenta': '\033[95m',
@@ -67,8 +65,33 @@ def colored_stars_balls(tree):
     return tree
 
 
-if __name__ == '__main__':
+def cli():
+    parser = argparse.ArgumentParser(prog="Python Christmas Tree by Chico Lucio from Ciencia Programada",
+                                     epilog="Ctrl-C interrupts the Christmas :-(")
+    parser.add_argument('-s', '--size', default=13, type=int,
+                        help="Tree height. If even it will be subtracted 1. If less than 7, considered 5. Default: 13")
+    parser.add_argument('-w', '--width', default=80, type=int,
+                        help="Screen width. Used to center the tree. Default: 80")
+    parser.add_argument('-t', '--terminal', action='store_true',
+                        help="Uses the terminal size to center the tree. -s and -w will be ignored")
+    args = parser.parse_args()
+
+    if args.terminal:
+        screen_width, height = os.get_terminal_size()
+        height -= 2
+    else:
+        height = args.size
+        screen_width = args.width
     while True:
-        time.sleep(random.uniform(.5, 1.5))
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print('\n'.join(colored_stars_balls(balls(tree()))))
+        try:
+            time.sleep(random.uniform(.1, 1))
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print('\n'.join(colored_stars_balls(balls(tree(height, screen_width)))))
+        except KeyboardInterrupt:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"\n{'Merry Christmas!!':^{screen_width}}", end='\n\n')
+            break
+
+
+if __name__ == '__main__':
+    cli()
